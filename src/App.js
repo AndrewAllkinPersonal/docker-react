0a121de661c4503694afc753ddf144614f033ed1
AndrewAllkinPersonal <andrewallkin@gmail.com>
2024-01-11 17:41:05 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first react.js app.
+          This is my first react.js app!
         </p>
         <a
           className="App-link"
